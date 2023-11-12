4128f7f29544e0a88eeab0c9889d88fd4cf388ff
Efrem-Yohanis <efremyohanis116@gmail.com>
2023-11-12 18:27:16 +0300

    code

--- cypress/e2e/Cypress_First_Test_Code.cy.js
+++ cypress/e2e/Cypress_First_Test_Code.cy.js
@@ -3,3 +3,42 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 })
+
+//Example with nameless function.
+
+describe('template spec', function(){
+ it('passes', function(){
+    cy.visit('https://example.cypress.io')
+ })
+})
+
+//Example using skiping describe block.
+
+describe.skip('template spec', function(){
+  it('passes', function(){
+   cy.visit('https://example.cypress.io')
+  })
+})
+
+// Example using skiping it block.
+
+describe('template spec', function(){
+  it.skip('passes', function(){
+  cy.visit('https://example.cypress.io')
+  })
+})
+
+// Example using only describe block.
+describe.only('template spec', function(){
+    it('passes', function(){
+     cy.visit('https://example.cypress.io')
+     })
+  })
+
+  // Example using only in it block.
+
+describe('template spec', function(){
+ it.only('passes', function(){
+  cy.visit('https://example.cypress.io')
+  })
+})
