0e1ed5eadf980f188d28ddfa197d8db0a17f90c9
Efrem-Yohanis-Anbese <efremyohanis116@gmail.com>
2024-01-12 21:54:00 +0300

    this is Test document

--- cypress/e2e/Test file for login page.cy.js
+++ cypress/e2e/Test file for login page.cy.js
@@ -1,7 +1,8 @@
 describe('Testing login functinalty', () => {
-  it('Verify that a user can successfully log in with valid credentials.', () => {
+  it.only('Verify that a user can successfully log in with valid credentials.', () => {
      // Visit the login page
     cy.visit('https://end-to-end-v1.onrender.com/')
+    cy.log(cy.url())
      // Enter valid username
     cy.get('#username').type('test')
      // Enter valid password
@@ -9,13 +10,29 @@ describe('Testing login functinalty', () => {
      // Click on the login button
     cy.get('button').click()
     // Assert that the URL has changed to the home page URL
-    cy.url().should('eq', 'https://end-to-end-v1.onrender.com/home/')
+    //cy.url().should('include', '/home/').and('contain','https:')  this is using imple
+
+    cy.url().then((value) => {
+      const expected_Url = 'https://end-to-end-v1.onrender.com/home/';
+      const actual_Url = value;
+      expect(actual_Url).to.equal(expected_Url);
+    });
+  
+    
+
+    cy.url().then((value) => {
+      const expectedUrl = 'https://end-to-end-v1.onrender.com/home/';
+      const actualUrl = value;
+      assert.equal(actualUrl, expectedUrl);
+    });
+
 
   })
 
   it('Verify that the system behavior with an invalid username and valid password.', () => {
       // Visit the login page
       cy.visit('https://end-to-end-v1.onrender.com/')
+    
       // Enter invalid username
      cy.get('#username').type('invalidusername')
       // Enter valid password
